adc430d8b5fc6e64a2749ee870cfd3f0af09b8c7
Auw Yang, Stefan <stefan_ay@yahoo.com>
2020-11-04 00:10:53 +0800

    Start using test case

--- src/test/java/com/stefanauwyang/blockone/studentenrollment/StudentEnrollmentAppTest.java
+++ src/test/java/com/stefanauwyang/blockone/studentenrollment/StudentEnrollmentAppTest.java
@@ -1,5 +1,7 @@
 package com.stefanauwyang.blockone.studentenrollment;
 
+import com.stefanauwyang.blockone.studentenrollment.db.models.Student;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +21,11 @@ public class StudentEnrollmentAppTest {
     private TestRestTemplate restTemplate;
 
     @Test
-    public void greetingShouldReturnDefaultMessage() throws Exception {
-        String str = restTemplate.getForObject("http://localhost:" + port + "/",
-                String.class);
+    public void studentsShouldReturnAllStudents() throws Exception {
+        Student student = restTemplate.getForObject("http://localhost:" + port + "/students/1",
+                Student.class);
+        Assert.assertEquals("Student should have id: 1", new Long(1), student.getId());
+        Assert.assertEquals("Michael", student.getFirstName());
     }
 
 }
--- src/test/java/com/stefanauwyang/blockone/studentenrollment/controllers/StudentControllerTest.java
+++ src/test/java/com/stefanauwyang/blockone/studentenrollment/controllers/StudentControllerTest.java
@@ -1,7 +1,10 @@
 package com.stefanauwyang.blockone.studentenrollment.controllers;
 
 import junit.framework.TestCase;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 public class StudentControllerTest extends TestCase {
 
 }
